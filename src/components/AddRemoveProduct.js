65e50c7508efd97cfe7235e172a2d73d7e2f3b0e
aragulshan <aragulshan321@gmail.com>
2023-11-03 11:25:42 +0500

    feat: update folder structure

---
+++ src/components/AddRemoveProduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddRemoveProduct = () => {
+  return (
+    <div>AddRemoveProduct</div>
+  )
+}
+
+export default AddRemoveProduct
---
+++ src/components/SearchInpu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchInpu = () => {
+  return (
+    <div>SearchInpu</div>
+  )
+}
+
+export default SearchInpu
